e1c102213f6e9064158c3bb37c7713d377b413da
sanglee1702 <43114738+sanglee1702@users.noreply.github.com>
2021-12-09 14:42:21 +0700

    cmt

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dbContext from './models';
-import AuthRouter from './routes/auth.routes';
-import * as dotenv from 'dotenv';
-import { envConfig } from './config/env.config';
-import logger from './logs/logger';
-import swaggerUi from 'swagger-ui-express';
-import * as swaggerDocument from './swagger/swagger.json';
-import { NotFoundError, ReporingError } from './utils/error';
-import ChallengeRouter from './routes/challenge.routes';
-import moment from 'moment';
-import ProductRouter from './routes/product.routes';
-import BannerRouter from './routes/banner.routes';
-import UserRouter from './routes/user.routes';
-import FileControllers from './controllers/file.controllers';
-import FilesRouter from './routes/file.routes';
-import ArticleRouter from './routes/article.routes';
-import GiftBoxRouter from './routes/giftbox.routes';
-import CommonRouter from './routes/common.routes';
-import PaymentRouter from './routes/payment.routes';
-import LocationRouter from './routes/location.routes';
-import DeliveryRouter from './routes/delivery.routes';
-import DiscountCodeRouter from './routes/discount.code.routes';
-import AdvertisementVideoRouter from './routes/advertisement.video.routes';
-import NotificationsRouter from './routes/notifications.routes';
-import admin, { initializeApp } from 'firebase-admin';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dbContext from "./models";
+import AuthRouter from "./routes/auth.routes";
+import * as dotenv from "dotenv";
+import { envConfig } from "./config/env.config";
+import logger from "./logs/logger";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger/swagger.json";
+import { NotFoundError, ReporingError } from "./utils/error";
+import moment from "moment";
+import FileControllers from "./controllers/file.controllers";
+import FilesRouter from "./routes/file.routes";
+import CommonRouter from "./routes/common.routes";
+import NotificationsRouter from "./routes/notifications.routes";
+import admin, { initializeApp } from "firebase-admin";
 
-const serviceAccount = require('../config/gofun-333404-firebase-adminsdk-jsff7-0b4e6780c7.json');
+const serviceAccount = require("../config/gofun-333404-firebase-adminsdk-jsff7-0b4e6780c7.json");
 
 dotenv.config();
 
-moment.updateLocale('en', {
+moment.updateLocale("en", {
   week: {
     dow: 1,
   },
@@ -40,23 +29,7 @@ moment.updateLocale('en', {
 const init = () => {
   const server = express();
 
-  const routes = [
-    AuthRouter,
-    ChallengeRouter,
-    ProductRouter,
-    BannerRouter,
-    UserRouter,
-    FilesRouter,
-    ArticleRouter,
-    GiftBoxRouter,
-    CommonRouter,
-    PaymentRouter,
-    LocationRouter,
-    DeliveryRouter,
-    DiscountCodeRouter,
-    AdvertisementVideoRouter,
-    NotificationsRouter,
-  ];
+  const routes = [AuthRouter, FilesRouter, CommonRouter, NotificationsRouter];
 
   server.use(cors());
   // Parse request of content-type - application/json
@@ -66,13 +39,13 @@ const init = () => {
 
   // route api
   routes.forEach((item) => {
-    server.use('/api', item);
+    server.use("/api", item);
   });
-  server.use('/files/**', FileControllers.getFile);
-  server.use('/bank-icon/**', FileControllers.getBankIcon);
+  server.use("/files/**", FileControllers.getFile);
+  server.use("/bank-icon/**", FileControllers.getBankIcon);
 
   // swagger
-  server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  server.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   // 404: Not found
   server.use(function (req, res, next) {
@@ -81,7 +54,7 @@ const init = () => {
       ...req.body,
     });
 
-    res.status(404).send(new NotFoundError('Không tìm thấy trang').toModel());
+    res.status(404).send(new NotFoundError("Không tìm thấy trang").toModel());
     next();
   });
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,133 +1,69 @@
-import { Model, ModelCtor, Sequelize } from 'sequelize';
-import dbConfig from '../config/db.config';
+import { Model, ModelCtor, Sequelize } from "sequelize";
+import dbConfig from "../config/db.config";
 import restAccountTable, {
   IAccountModels,
   ICreateAccountModels,
-} from './accounts.models';
+} from "./accounts.models";
 import restPointsHistoriesTable, {
   IPointsHistoriesModels,
   ICreatePointsHistoriesModels,
-} from './points.history.models';
+} from "./points.history.models";
 import restPointsTable, {
   IPointsModels,
   ICreatePointsModels,
-} from './points.models';
-import restChallengesTable, {
-  IChallengesModels,
-  ICreateChallengesModels,
-} from './challenges.models';
+} from "./points.models";
 import restCompanyTable, {
   ICompanyModels,
   ICreateCompanyModels,
-} from './company.models';
+} from "./company.models";
 import restGroupsTable, {
   ICreateGroupsModels,
   IGroupsModels,
-} from './groups.models';
+} from "./groups.models";
 import restPaymentHistoriesTable, {
   ICreatePaymentHistoriesModels,
   IPaymentHistoriesModels,
-} from './payment.histories.models';
-import restOTPTable, { ICreateOTPModels, OTPModels } from './otp.models';
+} from "./payment.histories.models";
+import restOTPTable, { ICreateOTPModels, OTPModels } from "./otp.models";
 import restUserChallengeTable, {
   ICreateUserChallengeModels,
   IUserChallengeModels,
-} from './user.challenge.models';
-import restUsersTable, { ICreateUserModels, IUserModels } from './users.models';
-import restArticleCategoryTable, {
-  IArticleCategoryModels,
-  ICreateArticleCategoryModels,
-} from './article.category.models';
-import restArticleTable, {
-  IArticleModels,
-  ICreateArticleModels,
-} from './article.models';
-import restBannerTable, {
-  IBannerModels,
-  ICreateBannerModels,
-} from './banner.models';
+} from "./user.challenge.models";
+import restUsersTable, { ICreateUserModels, IUserModels } from "./users.models";
 import restCompanyEmployeeTable, {
   ICompanyEmployeeModels,
   ICreateCompanyEmployeeModels,
-} from './company.employee.models';
-import restDeliveryAddressTable, {
-  IDeliveryAddressModels,
-  ICreateDeliveryAddressModels,
-} from './delivery.address.models';
-import restDeliveryTable, {
-  IDeliveryModels,
-  ICreateDeliveryModels,
-} from './delivery.models';
-import restDeliveryProductTable, {
-  IDeliveryProductModels,
-  ICreateDeliveryProductModels,
-} from './delivery.product.models';
-import restHistoryRunTable, {
-  IHistoryRunModels,
-  ICreateHistoryRunModels,
-} from './history.run.models';
-import restMainCategoryTable, {
-  ICreateMainCategoryModels,
-  IMainCategoryModels,
-} from './product.main.category.models';
-import restProductCategoryTable, {
-  ICreateProductCategoryModels,
-  ProductCategoryModels,
-} from './product.category.models';
-import restProductCommentTable, {
-  ICreateProductCommentModels,
-  IProductCommentModels,
-} from './product.comment.models';
-import restProductTable, {
-  ICreateProductModels,
-  IProductModels,
-} from './product.models';
+} from "./company.employee.models";
 import restGroupUserTable, {
   ICreateGroupUserModels,
   IGroupUserModels,
-} from './user.groups.models';
+} from "./user.groups.models";
 import restWishlistTable, {
   ICreateWishlistModels,
   IWishlistModels,
-} from './wishlist.models';
+} from "./wishlist.models";
 import restDiscountCodeTable, {
   ICreateDiscountCodeModels,
   IDiscountCodeModels,
-} from './discount.code.models';
-import restGiftOpeningHistoryTable, {
-  ICreateGiftOpeningHistoryModels,
-  IGiftOpeningHistoryModels,
-} from './gift.opening.history.models';
-import restArticleWishlistTable, {
-  IArticleCreateWishlistModels,
-  IArticleWishlistModels,
-} from './article.wishlist.models';
+} from "./discount.code.models";
 import restProvinceTable, {
   ICreateProvinceModels,
   IProvinceModels,
-} from './province.models';
+} from "./province.models";
 import restDistrictTable, {
   ICreateDistrictModels,
   IDistrictModels,
-} from './district.models';
-import restWardTable, { ICreateWardModels, IWardModels } from './ward.models';
-import restFileTable, { ICreateFileModels, IFileModels } from './files.models';
-import restAdvertisementVideoTable, {
-  IAdvertisementVideoModels,
-  ICreateAdvertisementVideoModels,
-} from './advertisement.video.models';
+} from "./district.models";
+import restWardTable, { ICreateWardModels, IWardModels } from "./ward.models";
+import restFileTable, { ICreateFileModels, IFileModels } from "./files.models";
 import restUserDiscountCodeTable, {
   ICreateUserDiscountCodeModels,
   IUserDiscountCodeModels,
-} from './user.discount.code.models';
+} from "./user.discount.code.models";
 import restNotificationsTable, {
   ICreateNotificationsModels,
   INotificationsModels,
-} from './notifications.models';
-import restCardLinkTable, {
-  ICardLinkModels,
-  ICreateCardLinkModels,
-} from './card.link.models';
+} from "./notifications.models";
 
 export interface IDBContext {
   databaseConfig: Sequelize;
@@ -138,9 +74,6 @@ export interface IDBContext {
     Model<IPointsHistoriesModels, ICreatePointsHistoriesModels>
   >;
   PointContext: ModelCtor<Model<IPointsModels, ICreatePointsModels>>;
-  ChallengensContext: ModelCtor<
-    Model<IChallengesModels, ICreateChallengesModels>
-  >;
   CompanyContext: ModelCtor<Model<ICompanyModels, ICreateCompanyModels>>;
   GroupsContext: ModelCtor<Model<IGroupsModels, ICreateGroupsModels>>;
   PaymentHistoriesContext: ModelCtor<
@@ -150,42 +83,11 @@ export interface IDBContext {
     Model<IUserChallengeModels, ICreateUserChallengeModels>
   >;
   OtpContext: ModelCtor<Model<OTPModels, ICreateOTPModels>>;
-  ArticleCategoryContext: ModelCtor<
-    Model<IArticleCategoryModels, ICreateArticleCategoryModels>
-  >;
-  ArticleContext: ModelCtor<Model<IArticleModels, ICreateArticleModels>>;
-  ArticleWishlistContext: ModelCtor<
-    Model<IArticleWishlistModels, IArticleCreateWishlistModels>
-  >;
-  BannerContext: ModelCtor<Model<IBannerModels, ICreateBannerModels>>;
   CompanyEmployeeContext: ModelCtor<
     Model<ICompanyEmployeeModels, ICreateCompanyEmployeeModels>
   >;
-  DeliveryAddressContext: ModelCtor<
-    Model<IDeliveryAddressModels, ICreateDeliveryAddressModels>
-  >;
-  DeliveryContext: ModelCtor<Model<IDeliveryModels, ICreateDeliveryModels>>;
-  DeliveryProductContext: ModelCtor<
-    Model<IDeliveryProductModels, ICreateDeliveryProductModels>
-  >;
-  HistoryRunContext: ModelCtor<
-    Model<IHistoryRunModels, ICreateHistoryRunModels>
-  >;
-  ProductMainCategoryContext: ModelCtor<
-    Model<IMainCategoryModels, ICreateMainCategoryModels>
-  >;
-  ProductCategoryContext: ModelCtor<
-    Model<ProductCategoryModels, ICreateProductCategoryModels>
-  >;
-  ProductContext: ModelCtor<Model<IProductModels, ICreateProductModels>>;
-  ProductCommentContext: ModelCtor<
-    Model<IProductCommentModels, ICreateProductCommentModels>
-  >;
   GroupUserContext: ModelCtor<Model<IGroupUserModels, ICreateGroupUserModels>>;
   WishlistContext: ModelCtor<Model<IWishlistModels, ICreateWishlistModels>>;
-  GiftOpeningHistoryContext: ModelCtor<
-    Model<IGiftOpeningHistoryModels, ICreateGiftOpeningHistoryModels>
-  >;
   ProvinceContext: ModelCtor<Model<IProvinceModels, ICreateProvinceModels>>;
   DistrictContext: ModelCtor<Model<IDistrictModels, ICreateDistrictModels>>;
   WardContext: ModelCtor<Model<IWardModels, ICreateWardModels>>;
@@ -193,16 +95,12 @@ export interface IDBContext {
   DiscountCodeContext: ModelCtor<
     Model<IDiscountCodeModels, ICreateDiscountCodeModels>
   >;
-  AdvertisementVideoContext: ModelCtor<
-    Model<IAdvertisementVideoModels, ICreateAdvertisementVideoModels>
-  >;
   UserDiscountCodeContext: ModelCtor<
     Model<IUserDiscountCodeModels, ICreateUserDiscountCodeModels>
   >;
   NotificationsContext: ModelCtor<
     Model<INotificationsModels, ICreateNotificationsModels>
   >;
-  CardLinksContext: ModelCtor<Model<ICardLinkModels, ICreateCardLinkModels>>;
 }
 
 const dbUri = `${dbConfig.dialect}://${dbConfig.DB_URL}/${dbConfig.DB}`;
@@ -220,7 +118,7 @@ const database = new Sequelize(dbUri, {
     idle: dbConfig.pool.idle,
   },
   define: {
-    charset: 'utf8mb4',
+    charset: "utf8mb4",
   },
 });
 
@@ -229,95 +127,33 @@ dbContext.databaseConfig = database;
 // function to drop existing tables and re-sync database
 dbContext.dropRestApiTable = () => {
   dbContext.databaseConfig.sync({ force: true }).then(() => {
-    console.log('restTutorial table just dropped and db re-synced.');
+    console.log("restTutorial table just dropped and db re-synced.");
   });
 };
 dbContext.UsersContext = restUsersTable(database);
 dbContext.AccountsContext = restAccountTable(database);
 dbContext.PointHistoriesContext = restPointsHistoriesTable(database);
 dbContext.PointContext = restPointsTable(database);
-dbContext.ChallengensContext = restChallengesTable(database);
 dbContext.CompanyContext = restCompanyTable(database);
 dbContext.GroupsContext = restGroupsTable(database);
 dbContext.PaymentHistoriesContext = restPaymentHistoriesTable(database);
 dbContext.UserChallengensContext = restUserChallengeTable(database);
 dbContext.OtpContext = restOTPTable(database);
-dbContext.ArticleCategoryContext = restArticleCategoryTable(database);
-dbContext.ArticleContext = restArticleTable(database);
-dbContext.BannerContext = restBannerTable(database);
 dbContext.CompanyEmployeeContext = restCompanyEmployeeTable(database);
-dbContext.DeliveryAddressContext = restDeliveryAddressTable(database);
-dbContext.DeliveryContext = restDeliveryTable(database);
-dbContext.DeliveryProductContext = restDeliveryProductTable(database);
-dbContext.HistoryRunContext = restHistoryRunTable(database);
-dbContext.ProductMainCategoryContext = restMainCategoryTable(database);
-dbContext.ProductCategoryContext = restProductCategoryTable(database);
-dbContext.ProductContext = restProductTable(database);
-dbContext.ProductCommentContext = restProductCommentTable(database);
 dbContext.GroupUserContext = restGroupUserTable(database);
 dbContext.WishlistContext = restWishlistTable(database);
 dbContext.DiscountCodeContext = restDiscountCodeTable(database);
-dbContext.AdvertisementVideoContext = restAdvertisementVideoTable(database);
-dbContext.GiftOpeningHistoryContext = restGiftOpeningHistoryTable(database);
-dbContext.ArticleWishlistContext = restArticleWishlistTable(database);
 dbContext.ProvinceContext = restProvinceTable(database);
 dbContext.DistrictContext = restDistrictTable(database);
 dbContext.WardContext = restWardTable(database);
 dbContext.FileContext = restFileTable(database);
 dbContext.UserDiscountCodeContext = restUserDiscountCodeTable(database);
 dbContext.NotificationsContext = restNotificationsTable(database);
-dbContext.CardLinksContext = restCardLinkTable(database);
 
 // get
-dbContext.DeliveryContext.hasMany(dbContext.DeliveryProductContext, {
-  foreignKey: 'deliveryId',
-  onDelete: 'SET NULL',
-});
-dbContext.ProductContext.hasOne(dbContext.WishlistContext, {
-  foreignKey: 'productId',
-  onDelete: 'SET NULL',
-});
-dbContext.ArticleContext.hasOne(dbContext.ArticleWishlistContext, {
-  foreignKey: 'articleId',
-  onDelete: 'SET NULL',
-});
 dbContext.AccountsContext.hasOne(dbContext.UsersContext, {
-  foreignKey: 'accountId',
-  onDelete: 'CASCADE',
-});
-dbContext.ProductCategoryContext.belongsTo(
-  dbContext.ProductMainCategoryContext,
-  {
-    foreignKey: 'mainCategoryId',
-    onDelete: 'SET NULL',
-  },
-);
-dbContext.ProductMainCategoryContext.hasMany(dbContext.ProductCategoryContext, {
-  foreignKey: 'mainCategoryId',
-  onDelete: 'SET NULL',
-});
-dbContext.ProductCategoryContext.belongsTo(
-  dbContext.ProductMainCategoryContext,
-  {
-    foreignKey: 'mainCategoryId',
-    onDelete: 'SET NULL',
-  },
-);
-dbContext.ProductCategoryContext.hasMany(dbContext.ProductContext, {
-  foreignKey: 'categoryId',
-  onDelete: 'SET NULL',
-});
-dbContext.ProductContext.belongsTo(dbContext.ProductCategoryContext, {
-  foreignKey: 'categoryId',
-  onDelete: 'SET NULL',
-});
-dbContext.ArticleContext.belongsTo(dbContext.ArticleCategoryContext, {
-  foreignKey: 'categoryId',
-  onDelete: 'SET NULL',
-});
-dbContext.ArticleCategoryContext.hasMany(dbContext.ArticleContext, {
-  foreignKey: 'categoryId',
-  onDelete: 'SET NULL',
+  foreignKey: "accountId",
+  onDelete: "CASCADE",
 });
 
 export default dbContext;
